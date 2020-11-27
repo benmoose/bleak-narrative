0f54bfb6b9af8e7bf88c53e85e6362e6877c7bab
Ben Hadfield <benjohnhadfield@gmail.com>
2020-11-27 16:45:12 +0000

    self-host fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import {Helmet} from 'react-helmet'
 import Layout from '../enhancers/layout'
 
+import 'react-bnb-gallery/dist/style.css'
 import '../public/css/reboot.css'
 import '../public/css/global.css'
-import 'react-bnb-gallery/dist/style.css'
 
 export default Layout(({ Component, pageProps }) => {
   return (
     <>
       <Helmet>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Noto+Sans+HK&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        <title>Bleak Narrative</title>
       </Helmet>
       <Component {...pageProps} />
     </>
