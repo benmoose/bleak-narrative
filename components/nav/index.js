bd0b1e101008c40179f5e03a3fdda31c2c28f474
Ben Hadfield <benjohnhadfield@gmail.com>
2021-01-11 23:24:41 +0000

    add page links to nav

--- components/nav/index.js
+++ components/nav/index.js
@@ -11,14 +11,22 @@ const Nav = () => {
   return (
     <nav className={styles.background}>
       <div className={styles.navContainer}>
-        <div className={styles.navSection} />
-        <div className={c(styles.navSection, styles.textCentre)}>
+        <div className={c(styles.navSection)}>
+          <div className={styles.pageLinkContainer}>
+            <Link href='/music' className={styles.pageLink}>Music</Link>
+            <Link href='/photos' className={styles.pageLink}>Art</Link>
+            <Link href='/story' className={styles.pageLink}>Stories</Link>
+          </div>
+        </div>
+
+        <div className={c(styles.navSection, styles.textCentre, styles.navSectionCentre)}>
           <NextLink href='/'>
             <a className={styles.brandLink}>
               <img src={bleakLogo} style={{ width: '62px' }} alt='Bleak Narrative' />
             </a>
           </NextLink>
         </div>
+
         <div className={c(styles.navSection, styles.textRight)}>
           <a target='_blank' href='https://soundcloud.com/bleaknarrative'>
             <img className={styles.soundcloud} src={soundcloudLogo} />
