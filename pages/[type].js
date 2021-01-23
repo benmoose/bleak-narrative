da693c85b389758fa372bbe55559c2d9ff3ba1ad
Ben Hadfield <benjohnhadfield@gmail.com>
2021-01-23 10:42:46 +0000

    add mobile nav

--- pages/[type].js
+++ pages/[type].js
@@ -24,7 +24,7 @@ const BleakPageByType = ({ results, page, totalPages }) => {
     id={doc.uid}
     type={doc.type}
     document={doc}
-                                  />)
+  />)
   return [TypeDesc, Feed]
 }
 
