8aa4722aad78bc17f2b68a7138883763055787a9
Ben Hadfield <benjohnhadfield@gmail.com>
2021-02-11 02:01:38 +0000

    use next/image on landing page

--- components/homePage/index.js
+++ components/homePage/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import styles from './homePage.module.css'
 import Grid from '@material-ui/core/Grid'
+import Image from 'next/image'
 
+import styles from './homePage.module.css'
 import DevTad from '../../public/img/websiteannouncementdoodle.png'
 
 export const HomePageJumbotron = () => {
   return (
     <Grid container spacing={4} alignItems='center'>
       <Grid item xs={12} sm={6}>
-        <img src={DevTad} className={styles.img} alt='Club culture banner' />
+        <Image priority src={DevTad} alt='Doodle of Tad frantically coding away...' width={380} height={380} className={styles.img} />
       </Grid>
       <Grid item xs={12} sm={6}>
         <p className={styles.subtitle}>Bleep Bloop</p>
