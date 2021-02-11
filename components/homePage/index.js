5bd71e895519645cd6ac73cebb393668a97a6ed7
Ben Hadfield <benjohnhadfield@gmail.com>
2021-02-11 18:32:59 +0000

    home page doodle

--- components/homePage/index.js
+++ components/homePage/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 import Grid from '@material-ui/core/Grid'
-import styles from './homePage.module.css'
 
-import Doodle from '../../public/img/homepagebanner.png'
+import styles from './homePage.module.css'
+import Doodle from '../../public/img/homedoodle.png'
 
 export const HomePageJumbotron = () => {
   return (
-    <Grid container spacing={4} alignItems='center'>
-      <Grid item xs={12} sm={6}>
-        <img src={Doodle} className={styles.img} alt='Club culture banner' />
+    <Grid container spacing={6} alignItems='center'>
+      <Grid item xs={12} sm={5}>
+        <Image priority src={Doodle} width={500} height={443} layout='' alt='Club culture banner' />
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <p className={styles.subtitle}>Bleep Bloop</p>
-        <h1 className={styles.title}>Short 'n snappy title, Andrea</h1>
-        <p>Subtitle here with stuff maybe..?</p>
+      <Grid item xs={12} sm={7}>
+        <p className={styles.subtitle}>Bleeps, Bloops, Sounds & Stories</p>
+        <h1 className={styles.title}>Welcum to Bleak Narrative</h1>
       </Grid>
     </Grid>
   )
