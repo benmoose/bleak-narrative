f18a3d4743f6cbc35cc84b94f53c5f32cd003f59
Ben Hadfield <benjohnhadfield@gmail.com>
2021-02-14 21:17:42 +0000

    add vercel to devDependencies, add privacy into re. tracking

--- pages/privacy.js
+++ pages/privacy.js
@@ -5,6 +5,7 @@ const PrivacyPage = () => {
       <p>Privacy policies, terms, and cookies are boring (unless the cookies are made of chocolate). Still, we have to have this section, or the authorities (!) might arrest us. Just kidding, that's an exaggeration. But seriously, they can shut down the website.</p>
       <p>Since we don't collect information from you (as in, you don't have to login anywhere to access our content or purchase products), we don't need to formally explain the privacy details. Rest assured, we're not obsessed with you nor storing your data.</p>
       <p>You have to consent to cookies (again, not edibles) though, and a banner will appear the first time you access our website, leading to this page. So read carefully what they have to say and enjoy your time here. If you want to undo your cookie-decisions, you have to Google how to do it cuz we don't know. :-)</p>
+      <p>We use <a target='_blank' rel='noreferrer' href='https://plausible.io/privacy-focused-web-analytics'>Plausible</a> to anonymously track usage of the website. They're small & privacy focused, you can read more about them <a target='_blank' rel='noreferrer' href='https://plausible.io/privacy-focused-web-analytics'>here</a>.</p>
     </main>
   )
 }
