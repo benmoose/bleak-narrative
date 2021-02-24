bcdf3058860f44ae1292e7b989f25e5573684f86
Ben Hadfield <benjohnhadfield@gmail.com>
2021-02-24 19:43:49 +0000

    fix image layout for imageAndText component

--- components/richText/index.js
+++ components/richText/index.js
@@ -2,11 +2,18 @@ import React from 'react'
 import PrismicDOM from 'prismic-dom'
 import styles from './richText.module.css'
 
+function linkResolver (doc) {
+  if (doc.isBroken) {
+    return ''
+  }
+  return `/${doc.type}/${doc.uid}`
+}
+
 const RichText = ({ content }) => {
   return (
     <div
       className={styles.container}
-      dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(content, null) }}
+      dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(content, linkResolver) }}
     />
   )
 }
