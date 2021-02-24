af5c21b46266b1e154b57652c9e55ffb04d49ef1
Ben Hadfield <benjohnhadfield@gmail.com>
2021-02-24 19:46:54 +0000

    fix image layout for imageAndText component (#36)

--- components/photoPage/photoAndText.js
+++ components/photoPage/photoAndText.js
@@ -5,8 +5,9 @@ import styles from './photoAndText.module.css'
 
 const PhotoAndText = ({ items, onClickHandler, urlToGalleryIndex }) => {
   return items.map((item, i) => {
-    const onImageClick = () => onClickHandler(urlToGalleryIndex[item.image.url])
-    const ImageC = getImageComponent({ imageURL: item.image.url, onImageClick })
+    const { dimensions, url } = item.image
+    const onImageClick = () => onClickHandler(urlToGalleryIndex[url])
+    const ImageC = getImageComponent({ dimensions, url, onImageClick })
     return (
       <div key={i} className={styles.container}>
         <div className={styles.imgContainer}>
@@ -20,8 +21,11 @@ const PhotoAndText = ({ items, onClickHandler, urlToGalleryIndex }) => {
   })
 }
 
-function getImageComponent ({ imageURL, imageAlt, onImageClick }) {
-  const ImgC = () => <Image layout='fill' src={imageURL} alt={imageAlt} className={styles.image} />
+function getImageComponent ({ url, imageAlt, onImageClick, dimensions }) {
+  if (!dimensions || !dimensions.width || !dimensions.height) {
+    throw Error(`image ${url} is missing required prop 'dimensions'`)
+  }
+  const ImgC = () => <Image width={dimensions.width} height={dimensions.height} src={url} alt={imageAlt} className={styles.image} />
   return onImageClick
     ? () => <a onClick={onImageClick}><ImgC /></a>
     : () => <ImgC />
