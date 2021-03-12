9c8038509c2ce825d3881b101f1fb652c9dae440
Ben Hadfield <benjohnhadfield@gmail.com>
2021-03-12 08:21:50 +0000

    add hosted video option (#51)

--- utils/env.js
+++ utils/env.js
@@ -1,3 +1,11 @@
 export function isServer () {
   return typeof window === 'undefined'
 }
+
+export function isProduction () {
+  return process.env.NODE_ENV === 'production'
+}
+
+export function isDevelopment () {
+  return process.env.NODE_ENV === 'development'
+}
