0b8565381a6019c3d37c90f6380871807d48b7b7
Ben Hadfield <benjohnhadfield@gmail.com>
2022-03-06 21:00:26 +0000

    Support short youtube urls (youtu.be) (#63)
    * update community page
    
    * support youtube urls

--- components/photoPage/video.js
+++ components/photoPage/video.js
@@ -1,5 +1,8 @@
 import styles from './onePhoto.module.css'
 
+const YOUTUBE_URL = 'youtube.com'
+const YOUTUBE_SHORT_URL = 'youtu.be'
+
 const Video = ({ items }) => {
   return items.map((item, i) => {
     return <VideoPlayer key={`${i}-${item.video.provider_name}`} url={item.video.embed_url} provider={item.video.provider_name} />
@@ -22,9 +25,19 @@ function getVideoPlayer ({ url, provider }) {
   throw Error(`No video player for provider ${provider}`)
 }
 
+function getYouTubeVideoID (youtubeUrl) {
+  const { host, pathname, searchParams } = new URL(youtubeUrl)
+  if (host !== YOUTUBE_URL && host !== YOUTUBE_SHORT_URL) {
+    throw Error(`URL '${youtubeUrl}' is not a YouTube URL`)
+  }
+  if (host === YOUTUBE_SHORT_URL) {
+    return pathname.slice(1)
+  }
+  return searchParams.get('v')
+}
+
 const YouTubePlayer = ({ url }) => {
-  const { searchParams } = new URL(url)
-  const videoID = searchParams.get('v')
+  const videoID = getYouTubeVideoID(url)
 
   if (!videoID) {
     throw Error(`Missing video ID for YouTube video '${url}'`)
